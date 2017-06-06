e691e64da7c0f7ca81a8ad160023ccef7d066a37
faner11 <fan233333@gmail.com>
2017-06-06 11:08:06 +0800

    需要修改

--- js/lb.js
+++ js/lb.js
@@ -1,5 +1,31 @@
- var mySwiper = new Swiper ('.swiper-container', {
-    direction: 'horizontal',
+ var mySwiper = new Swiper ('#lba', {
     loop: true,
     pagination: '.swiper-pagination',
-    })
+    });
+
+ var zthd = new Swiper ('#lbb', {
+     resistanceRatio : 0,
+ })
+
+
+
+// 搜索框
+ $('#ssk').on('input',function () {
+     var str = $(this).val();
+     $.ajax({
+         type:'GET',
+         url:"http://api.dagoogle.cn/music/music-and-lrc",
+         data:{count:8,keyword:str},
+         dataType:'jsonp',
+         jsonp:'callback',
+         jsonpCallback:'fn',
+         success:function (res) {
+             let data = res.data;
+             let txt = "";
+             $.each(data,function (index,itme) {
+                 txt += `<li><p>${itme.filename}</p></li>`;
+                 $('.recUl').html(txt)
+             })
+         }
+     })
+ })
